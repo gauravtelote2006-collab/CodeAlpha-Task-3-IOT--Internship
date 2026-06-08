32667c6a4416a41ca96d5b0809f916ee0e0ec841
Gaurav Telote <gauravtelote2006@gmail.com>
2026-06-08 18:21:31 +0530

    Add files via upload

---
+++ esp32-Temperature and Humidity.ino
@@ -0,0 +1,22 @@
+#include “DHT.h”
+ #define DHT11PIN 16
+ 	 
+ DHT dht(DHT11PIN, DHT11);
+ 	void setup()
+ 	{
+ 	 
+ 	Serial.begin(115200);
+ 	dht.begin();
+ 	}
+ 	 
+ 	void loop()
+ 	{
+ 	float humi = dht.readHumidity();
+ 	float temp = dht.readTemperature();
+ 	Serial.print(“Temperature: “);
+ 	Serial.print(temp);
+ 	Serial.print(“ºC “);
+ 	Serial.print(“Humidity: “);
+ 	Serial.println(humi);
+ 	delay(1000);
+ 	}
